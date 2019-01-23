e2a2fc77457dc4222938db67dbfab19cefba3f6c
Stefan Pasasopovicstssasde <a123stefanpopovic1987@outlook.com>
2019-01-23 06:45:54 +0100

    Commit changes to local branch.

---
+++ WMProducts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WMProducts.Startup))]
+namespace WMProducts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
